f099390d09131fb1d50e16fb247652b0257db030
Didier Emmanuel TAVARES <didier.tavares@gmail.com>
2026-09-08 00:47:33 -0300

    feat: add product catalog data to the application

---
+++ miniprojetoS07/src/data/produtos.js
@@ -0,0 +1,72 @@
+const catálogoProdutos = [
+  {
+    "id": 1,
+    "nome": "Encordoamento Guitarra 0.10 Nickel Wound",
+    "tipo": "Acessórios",
+    "preco": 65.90,
+    "quantidade": ""
+  },
+  {
+    "id": 2,
+    "nome": "Guitarra Elétrica Stratocaster Sunburst",
+    "tipo": "Instrumentos de Corda",
+    "preco": 6000.00,
+    "quantidade": ""
+  },
+  {
+    "id": 3,
+    "nome": "Pedal de Efeito Overdrive Vintage",
+    "tipo": "Pedais e Efeitos",
+    "preco": 420.00,
+    "quantidade": ""
+  },
+  {
+    "id": 4,
+    "nome": "Violão Aço Electro-Acústico Natural",
+    "tipo": "Instrumentos de Corda",
+    "preco": 1850.00,
+    "quantidade": ""
+  },
+  {
+    "id": 5,
+    "nome": "Amplificador para Guitarra 30W RMS",
+    "tipo": "Amplificadores",
+    "preco": 980.50,
+    "quantidade": ""
+  },
+  {
+    "id": 6,
+    "nome": "Cabo P10/P10 Blindado 5 Metros",
+    "tipo": "Cabos e Conectores",
+    "preco": 89.90,
+    "quantidade": ""
+  },
+  {
+    "id": 7,
+    "nome": "Contrabaixo Ativo 4 Cordas Preto",
+    "tipo": "Instrumentos de Corda",
+    "preco": 5800.00,
+    "quantidade": ""
+  },
+  {
+    "id": 8,
+    "nome": "Kit de Palhetas Sortidas 1.0mm (Pacote com 12)",
+    "tipo": "Acessórios",
+    "preco": 35.00,
+    "quantidade": ""
+  },
+  {
+    "id": 9,
+    "nome": "Piano Digital 88 Teclas Pesadas",
+    "tipo": "Teclado",
+    "preco": 5300.00,
+    "quantidade": ""
+  },
+  {
+    "id": 10,
+    "nome": "Slide Bottleneck de Vidro Temperado",
+    "tipo": "Acessórios",
+    "preco": 38.00,
+    "quantidade": ""
+  }
+]
