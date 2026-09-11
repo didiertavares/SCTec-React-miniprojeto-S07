a96a3a03035792763f159e53e8d5717ad50f1f9b
Didier Emmanuel TAVARES <didier.tavares@gmail.com>
2026-09-11 14:40:55 -0300

    feat: set up routing debut with BrowserRouter

--- miniprojetoS07/src/App.jsx
+++ miniprojetoS07/src/App.jsx
@@ -1,6 +1,11 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Carrinho from './pages/Carrinho'
+
+import Carrinho from './pages/Carrinho'
+import Pagamento from './pages/Pagamento'
+import Sucesso from './pages/Sucesso'
+import Falha from './pages/Falha'
 
 
 function App() {
