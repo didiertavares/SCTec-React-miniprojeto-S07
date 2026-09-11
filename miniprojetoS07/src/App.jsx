9c409bc4b40da9858084d59dc999a69414d7f93c
Didier Emmanuel TAVARES <didier.tavares@gmail.com>
2026-09-11 20:58:42 -0300

    feat: implement routing for Carrinho, Pagamento, Falha, and Sucesso pages

--- miniprojetoS07/src/App.jsx
+++ miniprojetoS07/src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import Carrinho from './pages/Carrinho'
 
 import Carrinho from './pages/Carrinho'
 import Pagamento from './pages/Pagamento'
@@ -13,9 +12,21 @@ function App() {
 
 
   return (
-    <>
-      <Carrinho />
-    </>
+    <Routes>
+
+      {/* Carrinho: conferência de produtos selecionados antes da compra*/}
+      <Route path="/carrinho" element={<Carrinho />} />
+
+      {/* Pagamento: interface de entrdad de dados bancários, pagamento & checkout */}
+      <Route path="/pagamento" element={<Pagamento />} />
+
+      {/* Página de erro: falha no processo de autenticação dos dados bancários */}
+      <Route path="/falha" element={<Falha />} />
+
+      {/* Página de sucesso: autenticação dos dados e aprovação do pagamento */}
+      <Route path="/sucesso" element={<Sucesso />} />
+
+    </Routes>
   )
 }
 
