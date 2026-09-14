6559dc855633d5eaa857852fa34a0e3efc23ea88
Didier Emmanuel TAVARES <didier.tavares@gmail.com>
2026-09-14 13:37:01 -0300

    feat: add useLocalStorage hook for managing local storage state

---
+++ miniprojetoS07/src/hooks/useLocalStorage.js
@@ -0,0 +1,21 @@
+function useLocalStorage(key, initialValue) {
+
+    const [value, setValue] = useState(() => {
+        const savedData = localStorage.getItem(key)
+
+        if (savedData === null) {
+            return initialValue
+        }
+
+        return JSON.parse(savedData)
+        
+    })
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value))
+    }, [key, value])
+
+    return [value, setValue]
+}
+
+export default useLocalStorage
